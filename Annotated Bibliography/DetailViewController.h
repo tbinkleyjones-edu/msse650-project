8f87d2fda0dd94c37b8c5c232d28c5c8d903d6c1
Tim <tbinkleyjones@regis.edu>
2014-05-25 22:45:45 -0600

    Added support for larger abstract and notes cells.

--- Annotated Bibliography/DetailViewController.h
+++ Annotated Bibliography/DetailViewController.h
@@ -8,9 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DetailViewController : UITableViewController
+@interface DetailViewController : UITableViewController <UITextViewDelegate>
 
 @property (strong, nonatomic) id detailItem;
 
-@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sourceTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *mediaTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *authorsLabel;
+@property (weak, nonatomic) IBOutlet UITextView *abstractTextView;
+@property (weak, nonatomic) IBOutlet UITextView *notesTextView;
+
 @end
--- Annotated Bibliography/DetailViewController.m
+++ Annotated Bibliography/DetailViewController.m
@@ -8,6 +8,8 @@
 
 #import "DetailViewController.h"
 
+#import "Entry.h"
+
 @interface DetailViewController ()
 - (void)configureView;
 @end
@@ -31,7 +33,12 @@
     // Update the user interface for the detail item.
 
     if (self.detailItem) {
-        self.detailDescriptionLabel.text = [self.detailItem description];
+        Entry *entry = self.detailItem;
+        self.sourceTitleLabel.text = entry.sourceTitle;
+        self.mediaTitleLabel.text = entry.mediaTitle;
+        self.authorsLabel.text = entry.authors;
+        self.abstractTextView.text = entry.abstract;
+        self.notesTextView.text = entry.notes;
     }
 }
 
@@ -39,7 +46,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    //self.navigationItem.rightBarButtonItem = self.editButtonItem;
     [self configureView];
 }
 
@@ -49,18 +56,37 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)setEditing:(BOOL)flag animated:(BOOL)animated
-{
-    [super setEditing:flag animated:animated];
-    if (flag == YES){
-        // Change views to edit mode.
-        NSLog(@"Change to edit mode.");
-    }
-    else {
-        // Save the changes if needed and change the views to noneditable.
-        NSLog(@"Change to view mode.");
-    }
-}
+//- (void)setEditing:(BOOL)flag animated:(BOOL)animated
+//{
+//    [super setEditing:flag animated:animated];
+//    if (flag == YES){
+//        // Change views to edit mode.
+//        NSLog(@"Change to edit mode.");
+//    }
+//    else {
+//        // Save the changes if needed and change the views to noneditable.
+//        NSLog(@"Change to view mode.");
+//    }
+//}
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"heightForRowAtIndexPath:%ld-%ld",(long)[indexPath section], (long)[indexPath row]);
+
+    // return a custom height for the abstract and note rows
+    CGFloat result;
+    switch ([indexPath section])
+    {
+        case 1:
+        case 2: {
+            result = 3 * tableView.rowHeight;
+            break;
+        }
+        default: {
+            result = tableView.rowHeight;
+        }
+    }
+    return result;
+}
 
 @end
